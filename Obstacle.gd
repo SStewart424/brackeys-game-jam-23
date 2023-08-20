fddad1f5be2b1abbb53c5b71f707468392eee4d7
Ben Gunton <bengunton@btinternet.com>
2023-08-20 20:44:20 +0100

    Connect sonar up to Obstacle to make it change colour

--- Obstacle.gd
+++ Obstacle.gd
@@ -3,19 +3,23 @@ extends Area2D
 @export var attack: int = 5
 @export var knockback: float = 500.0
 
+@onready var _animation = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+    pass
 
 
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		var knockbackDirection = (body.global_position - global_position).normalized()
-		body.take_damage(attack, knockbackDirection, knockback)
-	queue_free()
+    if body.is_in_group("Player"):
+        var knockbackDirection = (body.global_position - global_position).normalized()
+        body.take_damage(attack, knockbackDirection, knockback)
+    queue_free()
 
+func _on_sonar_sonar_activated():
+    _animation.play("detected")
