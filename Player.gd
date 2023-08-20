ac49b5c6edc579adc5b28acba355f184399e1559
Ben Gunton <bengunton@btinternet.com>
2023-08-20 17:14:37 +0100

    Add floaty animation

--- Player.gd
+++ Player.gd
@@ -4,23 +4,31 @@ extends CharacterBody2D
 @export var acceleration: float = 500.0
 @export var friction: float = 100.0
 
-var input: Vector2 = Vector2.ZERO
+@onready var _animated_sprite = $AnimationPlayer
+
+
+
+func _ready():
+    _animated_sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
-	player_movement(delta)
+    player_movement(delta)
 
 func get_input() -> Vector2:
-	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	return input.normalized()
+    var input: Vector2 = Vector2.ZERO
+    
+    input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+    input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+    return input.normalized()
+
 
 func player_movement(delta: float) -> void:
-	input = get_input()
+    var input: Vector2 = get_input()
 
-	if input == Vector2.ZERO:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-	else:
-		velocity = velocity.move_toward(input * max_speed, acceleration * delta)
+    if input == Vector2.ZERO:
+        velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+    else:
+        velocity = velocity.move_toward(input * max_speed, acceleration * delta)
 
-	move_and_slide()
+    move_and_slide()
 
