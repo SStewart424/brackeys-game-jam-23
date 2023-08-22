879cd960696c5fd9a12b8bd79cc3c4d8aa1c052a
Ben Gunton <bengunton@btinternet.com>
2023-08-22 19:37:35 +0100

    Move thigns around

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var bubble = preload("res://Bubble.tscn")
+var bubble = preload("res://Weapons/Bubble.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
     pass # Replace with function body.
