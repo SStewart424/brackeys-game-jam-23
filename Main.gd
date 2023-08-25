787ac89c9b6e7a00664dd746744f32e8fec57bca
Sean Stewart <sstewart424@gmail.com>
2023-08-25 17:44:06 +0200

    enemy spawning

--- Main.gd
+++ Main.gd
@@ -1,12 +1,29 @@
 extends Node2D
 
+var enemyScene = preload("res://Units/EnemyShark.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var spawn_interval = 3
+
 func _ready():
-    pass # Replace with function body.
+    $EnemySpawnTimer.set_wait_time(spawn_interval)
+    $EnemySpawnTimer.start()
 
+func spawn_enemy():
+    var player_pos = $Player.global_position
+    var spawn_radius = 400
+    var angle = randf_range(0, 360)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
+    # Convert angle to radians
+    var radians = deg_to_rad(angle)
 
+    # Calculate spawn position
+    var spawn_x = player_pos.x + spawn_radius * cos(radians)
+    var spawn_y = player_pos.y + spawn_radius * sin(radians)
+
+    var enemy = enemyScene.instantiate()
+    add_child(enemy)
+    enemy.global_position = Vector2(spawn_x, spawn_y)
+
+func _on_enemy_spawn_timer_timeout():
+    print("spawner timedout")
+    spawn_enemy()
